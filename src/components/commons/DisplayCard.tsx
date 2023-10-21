61a995a0032c3a086c034693072a1eea5535a476
gideonekekeke <gideonekeke64@gmail.com>
2023-10-21 17:03:14 +0100

    added the card

--- src/components/commons/DisplayCard.tsx
+++ src/components/commons/DisplayCard.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { AiFillEye } from "react-icons/ai";
 
 const DisplayCard = () => {
-  return (
-    <div className='w-[100px] bg-white text-black min-h-[100px] rounded-sm'>DisplayCard</div>
-  )
-}
+	return (
+		<div className='w-[270px] p-[20px] flex-1 bg-white text-primary min-h-[100px]  rounded-[10px] '>
+			<div className='flex items-center justify-between'>
+				<div className="flex">
+					<div className='h-[30px] w-[30px] bg-secondary rounded-[50%] mr-3'></div>
+					<div className='font-bold text-[17px] text-black mr-2'>
+						Wallet Balance
+					</div>
+				</div>
 
-export default DisplayCard
+				<div className='text-[20px]'>
+					<AiFillEye />
+				</div>
+			</div>
+			<div className="text-cyan-900 text-2xl font-bold font-['Open Sans']">
+				#5,000
+			</div>
+		</div>
+	);
+};
+
+export default DisplayCard;
--- src/pages/Agent/AgentOverView.tsx
+++ src/pages/Agent/AgentOverView.tsx
@@ -3,7 +3,9 @@ import DisplayCard from "../../components/commons/DisplayCard";
 
 const AgentOverView = () => {
 	return <div>
-		<div>
+		<div className="flex gap-5 flex-wrap">
+			<DisplayCard  />
+			<DisplayCard  />
 			<DisplayCard  />
 		</div>
 	</div>;
